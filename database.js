6c3d36ed58cc4d6ee31a85b3278aabb1b96dca9d
Jasmeen241 <jasnoorkaursidhu2002@gmail.com>
2021-11-12 12:27:12 +0530

    adding changes to database.js

--- database.js
+++ database.js
@@ -1,10 +1,37 @@
-let Database = {
-    cindy: {
+let Database = [
+    {   id : 1,
+        username : 'cindy',
+        email : "cindy@gmail.com",
+        password : "test",
         reminders: [{id: 1, title: "abc", description: "abcabc", completed: false}]
     },
-    alex: {
-        reminders: []
-    } 
-}
+    
+]
 
-module.exports = Database;
+const userModel = {
+    findOne: (email) => {
+      const user = Database.find((user) => user.email === email);
+      if (user) {
+        return user;
+      }
+      throw new Error(`Couldn't find user with email: ${email}`);
+    },
+    findById: (id) => {
+      const user = Database.find((user) => user.id === id);
+      if (user) {
+        return user;
+      }
+      throw new Error(`Couldn't find user with id: ${id}`);
+    },
+    add: (email, password) => {
+      Database.push({
+        id : Database.length + 1,
+        email : email,
+        password : password,
+        reminders : []
+      })
+    }
+  };
+  
+
+module.exports = { Database, userModel};
